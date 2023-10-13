6085b4939eb8aee465882c9c349cf95765624374
jorgeochipinti97 <jorgeochipinti97@gmail.com>
2023-10-12 23:56:45 -0300

    2

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,16 +1,16 @@
 export const nav = [
-	{
-		title: 'Home',
-		slug: '/',
-	},
+	// {
+	// 	title: 'Home',
+	// 	slug: '/',
+	// },
 	// {
 	// 	title: 'Blog',
 	// 	slug: '/blog',
 	// },
-	{
-		title: 'Sobre Nosotros',
-		slug: '/company/about',
-	},
+	// {
+	// 	title: 'Sobre Nosotros',
+	// 	slug: '/company/about',
+	// },
 	// {
 	// 	title: 'Contact',
 	// 	slug: '/company/contact',
